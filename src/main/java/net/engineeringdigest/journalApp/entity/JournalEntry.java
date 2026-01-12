02b74e63fc6f33dc89a09f6a23b33c6a58be3bf9
Smriti <=>
2026-01-13 02:07:16 +0530

    first commit

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,31 @@
+package net.engineeringdigest.journalApp.entity;
+
+
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection ="journal_entries")
+//@Getter
+//@Setter
+//@AllArgsConstructor
+@NoArgsConstructor
+//@ToString
+//@EqualsAndHashCode
+//@Builder
+@Data
+public class JournalEntry {
+    @Id
+    private ObjectId id;
+
+    @NonNull
+    private String title;
+
+    private String content;
+
+    private LocalDateTime date;
+
+}
---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,46 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@ActiveProfiles("dev")
+public class UserDetailsServiceImplTests {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @Disabled
+    void loadUserByUsername(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("jdfr").roles(new ArrayList<>()).build());
+     userDetailsService.loadUserByUsername("ram");
+
+        UserDetails user = userDetailsService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+
+}
